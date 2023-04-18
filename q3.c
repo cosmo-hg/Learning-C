b5c56be44b3558cf547d8bc6e9d675a72fd531cf
HARSH GUPTA <harshgupta@MacBook-Air.local>
2023-04-18 20:55:52 +0530

    Added q3.c file

---
+++ q3.c
@@ -0,0 +1,16 @@
+//to calculate the factorial of a number
+#include<stdio.h>
+int fact(int n)
+{
+    if (n==1)
+    return 1;
+    else 
+    return n*fact(n-1);
+}
+int main()
+{
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    printf("Factorial of a number %d is %d",n, fact(n));
+}
